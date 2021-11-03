7f38f29876fffe571e7df2f1bd7a9393a3b73167
Bogdan Mihaiciuc <bogdanmihaiciuc@gmail.com>
2021-11-03 15:00:13 +0200

    Add initializer for events and serialized objects

--- src/BMWindow/BMKeyboardShortcut.js
+++ src/BMWindow/BMKeyboardShortcut.js
@@ -1,6 +1,6 @@
-import { NO } from "../Core/BMCoreUI";
-
 // @ts-check
+
+import { NO } from "../Core/BMCoreUI";
 
 // @type BMKeyboardShortcutModifier
 
@@ -101,7 +101,7 @@ BMKeyboardShortcut.prototype = {
     preventsDefault: NO, // <Boolean>
 
     /**
-     * Initializes this keyboard shortcut with the given key and optional modifiers, as well as
+     * Designated initializer. Initializes this keyboard shortcut with the given key and optional modifiers, as well as
      * the target and action that will handle it.
      * 
      * In order to be triggered, this keyboard shortcut must be registered with an event handler such as a window
@@ -129,6 +129,88 @@ BMKeyboardShortcut.prototype = {
         }
 
         return this;
+    },
+
+    /**
+     * Initializes this keyboard shortcut with the given keyboard event. If the event includes the control or command keys, these may be converted
+     * into a system key modifier based on the current platform.
+     * @param event <KeyboardEvent>                                     The keyboard event from which this keyboard shortcut should be initialized.
+     * {
+     * 	@param target <AnyObject>										The object that will handle this keyboard shortcut action.
+     * 	@param action <String>											The name of a method on the target object that will be invoked when this 
+     * 																	keyboard shortcut is triggered. That method will receive the keyboard event as its single parameter.
+     *  @param preventsDefault <Boolean, nullable>                      Defaults to `NO`. When set to `YES`, the default action of the event that triggers this keyboard shortcut will be prevented.
+     * }
+     * 
+     */
+    initWithKeyboardEvent(event, {target, action, preventsDefault} = {}) {
+        const modifiers = [];
+        if (event.shiftKey) {
+            modifiers.push(BMKeyboardShortcutModifier.Shift);
+        }
+        if (event.altKey) {
+            modifiers.push(BMKeyboardShortcutModifier.Option);
+        }
+        if (event.metaKey) {
+            if (BMKeyboardShortcutModifier.System.value == BMKeyboardShortcutModifier.Command.value) {
+                modifiers.push(BMKeyboardShortcutModifier.System);
+            }
+            else {
+                modifiers.push(BMKeyboardShortcutModifier.Command);
+            }
+        }
+        if (event.controlKey) {
+            if (BMKeyboardShortcutModifier.System.value == BMKeyboardShortcutModifier.Control.value) {
+                modifiers.push(BMKeyboardShortcutModifier.System);
+            }
+            else {
+                modifiers.push(BMKeyboardShortcutModifier.Control);
+            }
+        }
+
+        return this.initWithKeyCode(event.code, {modifiers, target, action, preventsDefault});
+    },
+
+    /**
+     * Initializes this keyboard shortcut with the given serialized object representation. A callback is used to
+     * convert from the target object ID to an actual target object.
+     * @param shortcut <Object>                 A serialized keyboard shortcut.
+     * {
+     *  @param targetID <Object ^(String)>      A callback that is invoked with the target ID and should return
+     *                                          the target object which will handle the keyboard shortcut.
+     * }
+     * @returns <Object>                        This keyboard shortcut.
+     */
+    initWithSerializedKeyboardShortcut(shortcut, {targetID: resolver}) {
+        if (shortcut._class != 'BMKeyboardShortcut') {
+            throw new Error(`Unknown keyboard shortcut class - "${shortcut._class}"`);
+        }
+
+        const target = resolver(shortcut._targetID);
+
+        const modifiers = shortcut._modifiers.map(modifier => {
+            return Object.values(BMKeyboardShortcutModifier).find(m => m.key == modifier)
+        });
+
+        return this.initWithKeyCode(shortcut._key, {modifiers, target, action: shortcut._action, preventsDefault: shortcut._preventsDefault});
+    },
+
+    /**
+     * Returns an object representation of this keyboard shortcut that can be used with `JSON.stringify` to 
+     * serialize this keyboard shortcut.
+     * @param ID <String>           A string that identifies the target object and can be used when recreating
+     *                              the keyboard shortcut instance.
+     * @returns <Object>            An object that can be used with `JSON.stringify`.
+     */
+    serializedKeyboardShortcutWithTargetID(ID) {
+        return {
+            _class: `BMKeyboardShortcut`,
+            _key: this._key,
+            _modifiers: this._modifiers.map(m => m.key),
+            _targetID: ID,
+            _action: this._action,
+            _preventsDefault: this.preventsDefault,
+        };
     }
 
 };
@@ -146,11 +228,43 @@ BMKeyboardShortcut.prototype = {
  * 	@param target <AnyObject>										The object that will handle this keyboard shortcut action.
  * 	@param action <String>											The name of a method on the target object that will be invoked when this 
  * 																	keyboard shortcut is triggered. That method will receive the keyboard event as its single parameter.
+ *  @param preventsDefault <Boolean, nullable>                      Defaults to `NO`. When set to `YES`, the default action of the event that triggers this keyboard shortcut will be prevented.
  * }
  * @return <BMKeyboardShortcut>                                     A keyboard shortcut.
  */
 BMKeyboardShortcut.keyboardShortcutWithKeyCode = function (key, args) {
     return (new BMKeyboardShortcut).initWithKeyCode(key, args);
+};
+
+
+/**
+ * Constructs and returns a keyboard shortcut with the given keyboard event. If the event includes the control or command keys, these may be converted
+ * into a system key modifier based on the current platform.
+ * @param event <KeyboardEvent>                                     The keyboard event from which this keyboard shortcut should be initialized.
+ * {
+ * 	@param target <AnyObject>										The object that will handle this keyboard shortcut action.
+ * 	@param action <String>											The name of a method on the target object that will be invoked when this 
+ * 																	keyboard shortcut is triggered. That method will receive the keyboard event as its single parameter.
+ *  @param preventsDefault <Boolean, nullable>                      Defaults to `NO`. When set to `YES`, the default action of the event that triggers this keyboard shortcut will be prevented.
+ * }
+ * 
+ */
+BMKeyboardShortcut.keyboardShortcutWithKeyboardEvent = function (event, args) {
+    return (new BMKeyboardShortcut).initWithKeyboardEvent(event, args);
+};
+
+/**
+ * Constructs and returns keyboard shortcut with the given serialized object representation. A callback is used to
+ * convert from the target object ID to an actual target object.
+ * @param shortcut <Object>                 A serialized keyboard shortcut.
+ * {
+ *  @param targetID <Object ^(String)>      A callback that is invoked with the target ID and should return
+ *                                          the target object which will handle the keyboard shortcut.
+ * }
+ * @returns <Object>                        A keyboard shortcut.
+ */
+BMKeyboardShortcut.keyboardShortcutWithSerializedKeyboardShortcut = function (shortcut, args) {
+    return (new BMKeyboardShortcut).initWithSerializedKeyboardShortcut(shortcut, args);
 };
 
 // @endtype
