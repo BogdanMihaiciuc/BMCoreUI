996c049141e38c94a8ea560162c0c59cf6d48f98
Bogdan Mihaiciuc <bogdanmihaiciuc@gmail.com>
2021-11-24 16:15:58 +0200

    Avoid use of delete operator

--- src/BMWindow/BMWindow.js
+++ src/BMWindow/BMWindow.js
@@ -1166,7 +1166,7 @@ BMWindow.prototype = BMExtend(Object.create(BMView.prototype), {
 									}
 									else {
 										node.style.display = nodeDisplay;
-										delete self._nodeDisplay;
+										self._nodeDisplay = undefined;
 									}
 
 									completionHandler();
@@ -1328,14 +1328,14 @@ BMWindow.prototype = BMExtend(Object.create(BMView.prototype), {
 			// Create a copy of the given node which will be used in the animation in place of the original node
 			const node = this._anchorNode;
 			
-			if ('_nodeDisplay' in self) {
+			if (typeof self._nodeDisplay != 'undefined') {
 				node.style.display = self._nodeDisplay;
 			}
 			
 			const rect = BMRectMakeWithNodeFrame(node);
 			const animationNode = node.cloneNode(YES);
 			
-			const nodeDisplay = ('_nodeDisplay' in self) ? self._nodeDisplay : node.style.display;
+			const nodeDisplay = (typeof self._nodeDisplay != 'undefined') ? self._nodeDisplay : node.style.display;
 			self._nodeDisplay = undefined;
 			
 			animationNode.style.position = 'fixed';
