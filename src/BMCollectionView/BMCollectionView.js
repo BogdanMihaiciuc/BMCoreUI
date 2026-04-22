8c3888993a6c934e46e28554af3d1e7c16cc3743
Bogdan Mihaiciuc <bogdanmihaiciuc@gmail.com>
2026-04-22 13:45:09 +0300

    Remove use of jQuery for cell event handlers

--- src/BMCollectionView/BMCollectionView.js
+++ src/BMCollectionView/BMCollectionView.js
@@ -2671,8 +2671,8 @@ BMCollectionView.prototype = BMExtend(BM_COLLECTION_VIEW_USE_BMVIEW_SUBCLASS ? O
 				event.target.classList.contains('BMCollectionViewCellEventHandler') || event.target.parentNode.classList.contains('BMCollectionViewCellEventHandler') ||
 				event.target.classList.contains('widget-foldingpanel-header') ||
                 event.target.hasAttribute('contenteditable') ||
-				// This a specific Thingworx workaround that depends on jQuery
-				(window.$ ? window.$(event.target).parents('.widget-dhxdropdown').length : false)) {
+				// This a specific Thingworx workaround
+				event.target.closest('.widget-dhxdropdown')) {
 			    return;
 			}
 		    
